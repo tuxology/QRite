c46a05d920d4885b6a98f913f47cefc450a39720
Tanushri Chakravorty <tanushri.c@gmail.com>
2015-02-08 16:38:57 -0500

    Basic tab support with fragments

---
+++ app/src/main/java/in/suchakra/qrite/MainActivity.java
@@ -0,0 +1,52 @@
+package in.suchakra.qrite;
+
+import android.app.ActionBar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import tablayout.SlidingTabLayout;
+
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MainFragmentPageAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Give the SlidingTabLayout the ViewPager
+        SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        // Center the tabs in the layout
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_information, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/in/suchakra/qrite/MainFragmentPageAdapter.java
@@ -0,0 +1,36 @@
+package in.suchakra.qrite;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Tanushri on 08-Feb-15.
+ */
+public class MainFragmentPageAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 2;
+    private String tabTitles[] = new String[] { "Info", "Config" };
+    private Context context;
+
+    public MainFragmentPageAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return InfoFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
