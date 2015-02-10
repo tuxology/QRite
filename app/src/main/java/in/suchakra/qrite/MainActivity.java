589c841d466d4bea0e28e4d78b8c22bbe15d1ec6
Suchakra Sharma <suchakrapani.sharma@polymtl.ca>
2015-02-10 16:25:39 -0500

    Add material flavor to UI Change color of tabs & toolbar, add icon

--- app/src/main/java/in/suchakra/qrite/MainActivity.java
+++ app/src/main/java/in/suchakra/qrite/MainActivity.java
@@ -1,14 +1,20 @@
 package in.suchakra.qrite;
 
 import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import tablayout.SlidingTabLayout;
 
@@ -18,6 +24,15 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        
+        toolbar.setLogo(R.drawable.toolbar_icon);
+        toolbar.setTitleTextAppearance(getApplicationContext(), Typeface.BOLD);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.accent_material_light));
+        
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new MainFragmentPageAdapter(getSupportFragmentManager(),
@@ -27,6 +42,15 @@ public class MainActivity extends ActionBarActivity {
         SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
         // Center the tabs in the layout
         slidingTabLayout.setDistributeEvenly(true);
+
+        // Customize tab color
+        slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.accent_material_dark);
+            }
+        });
+        
         slidingTabLayout.setViewPager(viewPager);
     }
 
--- app/src/main/java/in/suchakra/qrite/MainFragment.java
+++ app/src/main/java/in/suchakra/qrite/MainFragment.java
@@ -1,15 +1,20 @@
 package in.suchakra.qrite;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Tanushri on 08-Feb-15.
  */
-// In this case, the fragment displays simple text based on the page
 public class MainFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
 
@@ -35,11 +40,31 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View info_view = inflater.inflate(R.layout.fragment_info, container, false);
         View config_view = inflater.inflate(R.layout.fragment_config, container, false);
-        if (mPage == 2){
-            return config_view;
-        }
+
+        /*
+        Context context = new ContextThemeWrapper(getActivity(), R.style.Base_Theme_AppCompat_Light);
+        // clone the inflater using the ContextThemeWrapper
+        LayoutInflater localInflater = inflater.cloneInContext(context);
+        // inflate using the cloned inflater, not the passed in default
+        View config_view = localInflater.inflate(R.layout.fragment_config, container, false);
+        View info_view = localInflater.inflate(R.layout.fragment_info, container, false);
+        */
+        Activity act = getActivity();
         if (mPage == 1){
+            Typeface font = Typeface.createFromAsset(act.getAssets(), "fontawesome.ttf" );
+            Button mNextbutton = (Button) info_view.findViewById( R.id.nextButton);
+            mNextbutton.setTypeface(font);
+            mNextbutton.setText("Next  "+"\uF0A9");
+            TextView mHeaderTextView = (TextView) info_view.findViewById(R.id.heading1View);
+            mHeaderTextView.setText("Enter Your Details");
             return info_view;
+        }
+        if (mPage == 2){
+            Typeface font = Typeface.createFromAsset(act.getAssets(), "fontawesome.ttf" );
+            Button button = (Button) config_view.findViewById( R.id.generateButton);
+            button.setTypeface(font);
+            button.setText("\uF029"+"  GENERATE");
+            return config_view;
         }
         return info_view;
     }
